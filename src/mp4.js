38ee7d534008c464160af2e3a7a09e309f14c7e6
ukyo <ukyo.web@gmail.com>
2012-03-11 18:03:31 +0900

    change comment

--- src/mp4.js
+++ src/mp4.js
@@ -1,5 +1,5 @@
 /**
- * @license mp4.js Copyright 2012 - Syu Kato <ukyo.web@gmail.com>
+ * mp4.js Copyright 2012 - Syu Kato <ukyo.web@gmail.com>
  * @version 0.2
  * Licensed under the Apache License, Version 2.0 (the "License");
  * http://www.apache.org/licenses/LICENSE-2.0

1602d396034320b77ceb5702cb37d988d0462e38
ukyo <ukyo.web@gmail.com>
2012-03-11 18:01:43 +0900

    rm

--- build.bat
+++
@@ -1,9 +0,0 @@
-:: minify with Closure Compiler.
-:: http://code.google.com/p/closure-compiler/
-java -jar compiler.jar^
-  --js=src/mp4.js^
-  --js=src/mp4.utils.js^
-  --js=src/mp4.descr.js^
-  --js=src/mp4.box.js^
-  --js=src/mp4.main.js^
-  --js_output_file=bin/mp4.min.js

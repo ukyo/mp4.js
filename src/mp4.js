60cfd0c909b97e947ed5918340bebdf4d8676fdc
ukyo <ukyo.web@gmail.com>
2012-03-12 23:16:16 +0900

    write version

--- src/mp4.js
+++ src/mp4.js
@@ -1,9 +1,9 @@
 /**
  * mp4.js Copyright 2012 - Syu Kato <ukyo.web@gmail.com>
- * @version 0.2
+ * @version 0.2.1
  * Licensed under the Apache License, Version 2.0 (the "License");
  * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 var mp4js = mp4js || {};
-mp4js.version = "0.2";
+mp4js.version = "0.2.1";

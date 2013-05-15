181c1094d3ba135dbfb5e08f22160fceccc53927
ukyo <ukyo.web@gmail.com>
2013-05-15 17:09:42 +0900

    remove

--- src/box/DataEntryBox.js
+++
@@ -1,6 +0,0 @@
-var DataEntryBox = FullBox.extend(function(parent, cls) {
-    this.init = function(params, flags) {
-        this.location = params.location;
-        parent.init.call(this, 0, flags);
-    };
-});
--- src/box/SampleTableBox.js
+++
@@ -1,8 +0,0 @@
-/*
-aligned(8) class SampleTableBox extends Box(‘stbl’) {
-}
-*/
-
-var SampleTableBox = Box.extend('stbl', function(parent, cls) {
-
-})
